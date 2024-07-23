b3377d4f2265bd5d2fba70d4648d6d98c70d7e51
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 01:12:04 +0200

    rename files

--- src/services/items/domain/services/ItemService.ts
+++
@@ -1,8 +0,0 @@
-import {useGetItems} from '../../use_cases/GetItems.ts'
-import {Item} from '../entities/Item.ts'
-
-export const ItemService = () => {
-  const {loading, error, data} = useGetItems()
-  const items: Item[] = data ? data.items : []
-  return {loading, error, items}
-}
