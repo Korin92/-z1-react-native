e9548dfd093b6c263b4b17b5c012c25937ec2f4e
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 01:09:37 +0200

    modify prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
   semi: false,
+  printWidth: 100,
+  tabWidth: 4,
 }
