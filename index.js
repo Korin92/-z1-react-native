a5eef29253495473bd20d90785ef1150a169fd51
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 19:57:22 +0200

    add service tracker player an add import tracker player to index

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import './gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
+import * as TrackPlayer from 'react-native-track-player/src/trackPlayer'
 
+TrackPlayer.registerPlaybackService(() => require('./service'))
 AppRegistry.registerComponent(appName, () => App)
