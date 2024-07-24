09b6e6457e9ac25d8d1f30ba27c7d143208bdd48
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 17:47:11 +0200

    refactor home

--- src/ui/features/home/Home.tsx
+++ src/ui/features/home/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet, FlatList } from 'react-native'
+import React, { useCallback } from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 import { Card } from '../../components/Card.tsx'
 import { RouteProp, useNavigation } from '@react-navigation/native'
 import { DetailScreenNavigationProp, TabNavParamList } from '../../../navigation/types.ts'
 import { ItemService } from '../../../services/items/domain/services/item-service.ts'
-import { filterItemsByCategory } from '../../../utils/filters-item-by-category.ts'
+import { Item } from '../../../services/items/domain/entities/Item.ts'
+import { FlashList } from '@shopify/flash-list'
+import { ItemByCategoryService } from '../../../services/items/domain/services/item-by-category-service.ts'
 
 type HomeProps = {
     route: RouteProp<TabNavParamList, keyof TabNavParamList>
@@ -13,30 +15,42 @@ type HomeProps = {
 export const Home: React.FC<HomeProps> = ({ route }) => {
     const navigation = useNavigation<DetailScreenNavigationProp>()
     const { categoryId } = route?.params || { categoryId: null }
-    const { loading, error, items } = ItemService()
+    const { loading, error, items } = categoryId ? ItemByCategoryService(categoryId) : ItemService()
     const isColumnLayout = route.name === 'All'
+    const numColumns = isColumnLayout ? 2 : 1
+
+    const handlePress = useCallback(
+        (id: string) => {
+            navigation.navigate('Detail', { id })
+        },
+        [navigation],
+    )
+
+    const renderItem = useCallback(
+        ({ item }: { item: Item }) => (
+            <Card
+                title={item.title}
+                author={item.author}
+                image={item.image}
+                type={item.category.title}
+                layout={isColumnLayout ? 'column' : 'row'}
+                onPress={() => handlePress(item.id)}
+            />
+        ),
+        [handlePress, isColumnLayout],
+    )
 
     if (loading) return <ActivityIndicator />
     if (error) return <Text>Error: {error.message}</Text>
 
-    const itemsByCategory = filterItemsByCategory(items, categoryId)
-
     return (
         <View style={styles.container}>
-            <FlatList
-                data={itemsByCategory}
+            <FlashList
+                data={items}
                 keyExtractor={item => item.id}
-                numColumns={isColumnLayout ? 2 : 1}
-                renderItem={({ item }) => (
-                    <Card
-                        title={item.title}
-                        author={item.author}
-                        image={item.image}
-                        type={item.category.title}
-                        layout={isColumnLayout ? 'column' : 'row'}
-                        onPress={() => navigation.navigate('Detail', { id: item.id })}
-                    />
-                )}
+                numColumns={numColumns}
+                renderItem={renderItem}
+                estimatedItemSize={200}
             />
         </View>
     )
