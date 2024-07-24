445c454309e117b0db337073a0857b214c79b562
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 17:47:33 +0200

    add conditional to service item by category

--- src/services/items/domain/services/item-by-category-service.ts
+++ src/services/items/domain/services/item-by-category-service.ts
@@ -1,6 +1,6 @@
 import { useGetItemsByCategory } from '../../use_cases/get-items-by-category.ts'
 
 export const ItemByCategoryService = (category?: string) => {
-    const { loading, error, items } = useGetItemsByCategory(category)
+    const { loading, error, items } = useGetItemsByCategory(category || '')
     return { loading, error, items }
 }
