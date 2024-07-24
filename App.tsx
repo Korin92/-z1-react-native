871e40ae2d53619a837e81f471b9a37cf0ff3a79
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-24 19:57:41 +0200

    add customTheme to NavigatorContainer and add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { ApolloProvider } from '@apollo/client'
 import client from './src/apollo/apollo-client'
-import { NavigationContainer } from '@react-navigation/native'
-import { globalStyles } from './src/styles/global-styles'
-import { View } from 'react-native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'react-native'
 import StackNavigator from './src/navigation/stack-navigator'
+import { background } from './src/styles/colors'
+
+const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: background,
+    },
+}
 
 function App(): React.JSX.Element {
     return (
         <ApolloProvider client={client}>
-            <View style={globalStyles.container}>
-                <NavigationContainer>
-                    <StackNavigator />
-                </NavigationContainer>
-            </View>
+            <StatusBar backgroundColor={background} barStyle="light-content" />
+            <NavigationContainer theme={MyTheme}>
+                <StackNavigator />
+            </NavigationContainer>
         </ApolloProvider>
     )
 }
