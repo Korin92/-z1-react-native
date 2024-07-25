331fd1b35f1be4e8d77236aba2d3e2447d9e6eff
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-25 19:39:56 +0200

    add types file to custom image component

---
+++ src/ui/components/image/types.ts
@@ -0,0 +1,14 @@
+export interface CustomImageStyleProps {
+    layout?: 'row' | 'column'
+    width?: string
+    height?: string
+    borderRadius?: string
+}
+
+export interface CustomImageProps {
+    layout?: 'row' | 'column'
+    source: string
+    width?: string
+    height?: string
+    borderRadius?: string
+}
