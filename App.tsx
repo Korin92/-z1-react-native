d8a78563b0e5970aa9ff8b5e831a73e7b61ca612
Korin92 <korin@MacBook-Air-atorreci.local>
2024-07-25 23:47:55 +0200

    init Trackplayer

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ApolloProvider } from '@apollo/client'
 import client from './src/apollo/apollo-client'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'react-native'
 import StackNavigator from './src/navigation/stack-navigator'
 import { background } from './src/styles/colors'
+import TrackPlayer from 'react-native-track-player'
 import './src/i18n'
 
 const MyTheme = {
@@ -16,6 +17,12 @@ const MyTheme = {
 }
 
 function App(): React.JSX.Element {
+    useEffect(() => {
+        TrackPlayer.setupPlayer().then(() => {
+            console.log('player ready')
+        })
+    }, [])
+
     return (
         <ApolloProvider client={client}>
             <StatusBar backgroundColor={background} barStyle="light-content" />
